19a90f6157389c05f696761d3ab595ef35f3134f
Evie-Mututa <evemututa29@gmail.com>
2023-09-23 17:44:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = "Evie Mututa"
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser +"!"
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, EVIE MUTUTA!"
+
+let shortGreeting = "Welcome, Evie!"
